engdoreis/ibex-demo-system

Add a SPI driver
Signed-off-by: Douglas Reis <doreis@lowrisc.org>

sw/common/demo_system.h
@@ -13,6 +13,9 @@
 #define UART0_BASE 0x80001000
 #define DEFAULT_UART UART_FROM_BASE_ADDR(UART0_BASE)
 
+#define SPI0_BASE 0x80003000
+#define DEFAULT_SPI SPI_FROM_BASE_ADDR(SPI0_BASE)
+
 #define GPIO0_BASE 0x80000000
 #define GPIO0 GPIO_FROM_BASE_ADDR(GPIO0_BASE)
 

ArfDesign-DB/minimal-ibex-soc

sw/common/demo_system.h
@@ -24,6 +24,9 @@
 #define PWM_BASE   0x80003000
 #define NUM_PWM_MODULES 12
 
+#define SPI0_BASE 0x80004000
+#define DEFAULT_SPI SPI_FROM_BASE_ADDR(SPI0_BASE)
+
 /**
  * Writes character to default UART. Signature matches c stdlib function
  * of the same name.